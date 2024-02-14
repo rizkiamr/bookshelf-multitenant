107610c62f91ba1d1faa4bb123712aa1568ef904
Muhammad Rizqy Amrullah <rizkii.amr@gmail.com>
2024-02-14 17:59:09 +0700

    feat: restrict root path

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Bookshelf" as the response body
 func home(w http.ResponseWriter, r *http.Request) {
+
+	// Check if the current request URL path exactly matches "/".
+	// If it doesn't, use the http.NotFound() function
+	// to send a 404 response to the client.
+	// Importantly, we then return from the handler.
+	// If we don't return the handler, the handler would keep executing
+	// and also write the "Hello from Bookshelf" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Bookshelf"))
 }
 
